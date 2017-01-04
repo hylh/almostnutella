7e47765624bfd0e4c7a98f9d2337cf8bd045a6f0
Roberth Hansen <roberth.hansen@uit.no>
2017-01-04 01:19:29 +0100

    Removed rest.py and put http server in nutella.py

--- nutella.py
+++ nutella.py
@@ -6,8 +6,21 @@ import socket
 import threading
 import signal
 import sys
-from rest import ThreadedHTTPServer, NodeHttpHandler
 from node import Node
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from SocketServer import ThreadingMixIn
+
+class NodeHttpHandler(BaseHTTPRequestHandler):
+    """ Gnutella like HTTP request handler """
+    def do_GET(self):
+        """ GET request for /neighbours """
+        if self.path == "/neighbours":
+            n_list = THIS_NODE.get_neighbour_list()
+            self.send_response(200)
+            self.end_headers()
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """ Handle requests in a separate thread """
 
 def parse_args():
     """ Parse command line arguments """
